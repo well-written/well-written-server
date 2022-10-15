86ed272f52f87317c5755c13cf35b15de75f49d9
XiNiHa <me@xiniha.dev>
2022-10-15 14:57:00 +0900

    CORS 및 GraphiQL 엔드포인트 등 이슈 처리

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use async_graphql::{http::GraphiQLSource, EmptyMutation, EmptySubscription, Schema};
 use async_graphql_axum::{GraphQLRequest, GraphQLResponse};
 use axum::{
+    http::Method,
     response::{self, IntoResponse},
     routing::get,
     Extension, Router,
 };
-use well_written_server::schema;
 use std::net::SocketAddr;
+use tower_http::cors::CorsLayer;
+use well_written_server::schema;
 
 async fn graphql_handler(
     schema: Extension<schema::AppSchema>,
@@ -18,23 +20,33 @@ async fn graphql_handler(
 async fn graphiql() -> impl IntoResponse {
     response::Html(
         GraphiQLSource::build()
-            .endpoint("http://localhost:3000/graphql")
+            .endpoint(&format!("{}/graphql", std::env::var("SERVER_URL").unwrap()))
             .finish(),
     )
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
+    dotenv::dotenv().ok();
+
     let schema = Schema::build(schema::QueryRoot, EmptyMutation, EmptySubscription).finish();
 
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET, Method::POST])
+        .allow_origin(tower_http::cors::Any);
+
     let app = Router::new()
+        .layer(cors)
         .route("/", get(|| async { "Hello, World!" }))
         .route("/graphql", get(graphiql).post(graphql_handler))
         .layer(Extension(schema));
 
-    println!("GraphiQL IDE: http://localhost:8080/graphql");
+    println!(
+        "GraphiQL IDE: {}/graphql",
+        std::env::var("SERVER_URL").unwrap()
+    );
 
-    let port = std::env::var("FLY_IO_PORT")
+    let port = std::env::var("SERVER_PORT")
         .ok()
         .and_then(|s| s.parse().ok())
         .unwrap_or(8080);
@@ -44,5 +56,7 @@ async fn main() {
     axum::Server::bind(&address)
         .serve(app.into_make_service())
         .await
-        .unwrap();    
+        .unwrap();
+
+    Ok(())
 }
